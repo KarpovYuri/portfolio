0e9bf134e6940c40c81bd98fd59104cbdca96e19
Karpov <yurick@bk.ru>
2023-08-06 14:04:19 +0300

    fix: Добавить скилы

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -26,15 +26,19 @@ export default {
   setup() {
     const list1 = ref([
       { skillName: 'HTML', startValue: 0, value: 95 },
-      { skillName: 'CSS', startValue: 0, value: 85 },
+      { skillName: 'CSS / Sass / SCSS', startValue: 0, value: 85 },
       { skillName: 'JavaScript', startValue: 0, value: 90 },
-      { skillName: 'TypeScript', startValue: 0, value: 60 }
+      { skillName: 'TypeScript', startValue: 0, value: 60 },
+      { skillName: 'React / Redux', startValue: 0, value: 80 },
+      { skillName: 'Vue / Nuxt / Vuex', startValue: 0, value: 60 }
     ]);
     const list2 = ref([
-      { skillName: 'React / Redux', startValue: 0, value: 80 },
-      { skillName: 'Vue / Nuxt / Vuex', startValue: 0, value: 60 },
-      { skillName: 'Node JS', startValue: 0, value: 75 },
-      { skillName: 'MongoDB', startValue: 0, value: 65 }
+      { skillName: 'Node JS', startValue: 0, value: 80 },
+      { skillName: 'Express', startValue: 0, value: 75 },
+      { skillName: 'Mongoose', startValue: 0, value: 75 },
+      { skillName: 'MongoDB', startValue: 0, value: 75 },
+      { skillName: 'PostgreSQL / MySQL', startValue: 0, value: 35 },
+      { skillName: 'Jest / Mocha', startValue: 0, value: 25 }
     ]);
     const lists = ref([{ field: list1 }, { field: list2 }]);
     setTimeout(() => {
