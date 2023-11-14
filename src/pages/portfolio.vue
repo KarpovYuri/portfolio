7ff61eb815fe8f7cbfd7fd35896d75d8af3c6dfd
Karpov <yurick@bk.ru>
2023-11-15 00:07:29 +0300

    feat: Добавить временную ссылку на работы на ГитХабе

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -8,6 +8,15 @@
         <PageTitle :title="portfolioTitle" />
         <PageSubtitle :subtitle="portfolioSubtitle" />
         <Projects />
+        <div class="link-wrapper">
+          <NuxtLink
+            class="link"
+            href="https://github.com/KarpovYuri?tab=repositories"
+            target="_blank"
+          >
+            Остальные работы
+          </NuxtLink>
+        </div>
       </div>
     </section>
   </div>
@@ -24,3 +33,33 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.link-wrapper {
+  margin-top: 10px;
+  text-align: center;
+}
+.link {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  color: var(--link-color);
+  position: relative;
+}
+
+.link::before {
+  content: '';
+  position: absolute;
+  height: 2px;
+  bottom: -4px;
+  background-color: var(--accent-color);
+  visibility: hidden;
+  width: 0;
+  transition: all 0.3s ease-in-out 0s;
+}
+
+.link:hover::before {
+  visibility: visible;
+  width: 176px;
+}
+</style>
